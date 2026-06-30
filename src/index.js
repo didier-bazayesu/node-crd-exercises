56bbc0825b705a586cb02fc99de459078353a97c
didier-bazayesu <didierbazayesu@gmail.com>
2026-06-30 19:18:46 +0200

    feat: final crud execises

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import dataRoutes  from './routes/dataRoutes.js'
-
 const app = express()
 app.use(express.json())
 app.use('/data', dataRoutes)
-app.get('/', ( __, res)=>{
-    res.status(200)
-})
-
-
 
 app.listen(process.env.PORT || 6000, ()=>{
     console.log(`The app is running on the port ${process.env.PORT}` )
