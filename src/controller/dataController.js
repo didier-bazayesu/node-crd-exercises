e8e51096a34cc0df06705237727f991896471f3f
didier-bazayesu <didierbazayesu@gmail.com>
2026-06-30 19:30:36 +0200

    feat: final crud execises

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,24 +1,10 @@
-import { trace } from "console";
-import fs from "fs/promises";
-import path from "path";
-import { json } from "stream/consumers";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const filePath = path.join(__dirname, "../database/data.json");
-
-async function readFile() {
-  const data = await fs.readFile(filePath, "utf-8");
-  return JSON.parse(data);
-}
-async function writeToFile(data) {
-  await fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf-8");
-}
+import { readToFIle } from "../service";
+import { writeToFile } from "../service";
 
 export async function getAll(__, res) {
   try {
-    const data = await readFile();
+    const data = await readToFIle();
     if (!data)
       return res.status(500).json({ message: "error in reading file" });
     return res.status(200).json(data);
@@ -30,7 +16,7 @@ export async function getAll(__, res) {
 export async function getById(req, res) {
   try {
      const { id } = req.params;
-    const data = await readFile();
+    const data = await readToFIle();
     const exsistData = data.filter((data) => data.id === Number(id));
     if(exsistData.length === 0) return res.status(404).json({message: "not data of thi id  found!"})
     return res.status(200).json(exsistData)
@@ -42,7 +28,7 @@ export async function getById(req, res) {
 
 export async function createData(req, res) {
   try {
-    const existData = await readFile();
+    const existData = await readToFIle();
     const { id, name, email, address } = req.body;
     if (existData.find((data) => data.id === Number(id)))
       return res
@@ -67,7 +53,7 @@ export async function updateData(req, res) {
     const { id } = req.params;
     const newData = req.body;
     if (!id) return res.status(500).json({ message: "no id provided" });
-    const data = await readFile();
+    const data = await readToFIle();
     const existUser = data.find(data => data.id === Number(id))
     if(!existUser) return res.status(404).json({    message:"User not found" });
     if(newData.id) return res.status(400).json({message: "You are not allowed to update the id"})
@@ -82,7 +68,7 @@ export async function updateData(req, res) {
 export  async function DeleteData(req , res){
      try {
     const { id } = req.params;
-    const data = await readFile();
+    const data = await readToFIle();
     const indexOfDelete = data.findIndex(data => data.id === Number(id));
     if(indexOfDelete) return res.status(404).json({message: "not data of thi id  found!"})
     const deletedUser = data[indexOfDelete]
